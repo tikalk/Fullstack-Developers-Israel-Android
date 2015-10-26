ee2d693b5844c407d90b86f538ee399af8843920
Shrek2000 <oren.kleks@gmail.com>
2015-10-26 23:35:36 +0200

    Add binding lib. Add image binder with glide

---
+++ app/src/main/java/com/tikalk/android/apps/fsil/binder/ImageBinder.java
@@ -0,0 +1,21 @@
+package com.tikalk.android.apps.fsil.binder;
+
+import android.content.Context;
+import android.databinding.BindingAdapter;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+/**
+ * Created by Oren on 26/10/2015.
+ */
+public class ImageBinder {
+    private ImageBinder() {
+    }
+
+    @BindingAdapter("imageUrl")
+    public static void setImageUrl(ImageView imageView, String url) {
+        Context context = imageView.getContext();
+        Glide.with(context).load(url).into(imageView);
+    }
+}
